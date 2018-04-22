e05b86770205245f86fe562604867c8cd3f35f94
Harsh Bhavsar <hbhavsar2110@yahoo.com>
2018-04-22 03:01:24 -0500

    Added ML Models

--- src/main/java/com/salvation/service/AnimalDataService.java
+++ src/main/java/com/salvation/service/AnimalDataService.java
@@ -2,9 +2,10 @@ package com.salvation.service;
 
 import java.util.List;
 
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -24,8 +25,14 @@ import lombok.extern.slf4j.Slf4j;
 @EnableScheduling
 public class AnimalDataService {
 
+	// @Autowired
+	// private StringRedisTemplate redisTemplate;
+
+	// @Autowired
+	private MultiLayerNetwork multiLayerNetwork;
+
 	@Autowired
-	private StringRedisTemplate redisTemplate;
+	private PullDataService pullDataService;
 
 	public void performManualProcessing() {
 
@@ -51,24 +58,55 @@ public class AnimalDataService {
 	}
 
 	public AnalyzeResponse analyzeAnimalData(AnalyzeRequest request) {
-		
-//		try {
-//			redisTemplate.opsForHash().put("blockchain", "1", "hello");
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
 		AnalyzeResponse response = new AnalyzeResponse();
 		BeanUtils.copyProperties(request, response);
-		response.setRiskFactor("LOW");
-		AnimalData data = new AnimalData();
-		data.setCondition("Sunny");
-		data.setCo2Emission(81.00);
-		data.setNitrogenLevel(55.0);
-		data.setPrecipitation(0);
-		data.setTemperature(56);
-		data.setSnowFall(17);
+		AnimalData data = pullDataService.getAnimalData(request);
+		// Calculate risk factor
+		String riskFactor = getRiskFactor(request.getAnimalName(), data);
+		response.setRiskFactor(riskFactor);
 		response.setData(data);
 		return response;
+	}
+
+	private String getRiskFactor(String animalName, AnimalData data) {
+		String riskFactor = "";
+		if (multiLayerNetwork != null) {
+			// get from trained model
+			multiLayerNetwork.computeGradientAndScore();
+			// prepare data.
+			// DataSet dataset = new DataSet(data);
+			// riskFactor = multiLayerNetwork.predict(dataSet);
+
+		} else {
+			if (data.getCondition().equals("Thunderstorm")) {
+				riskFactor = "HIGH";
+			} else {
+				riskFactor = "LOW";
+			}
+		}
+		return riskFactor;
+	}
+
+	public MultiLayerNetwork multiLayerNetwork() {
+		try {
+			// Nd4jBackend.load();
+
+			// FileInputStream modelConfigStream = new FileInputStream(new
+			// ClassPathResource(ECO_SAVIOR_MODEL_CONFIG).getFile());
+			// FileInputStream modelFileStream = new FileInputStream(new
+			// ClassPathResource(ECO_SAVIOR_MODEL_H5).getFile());
+			// MultiLayerNetwork network =
+			// KerasModelImport.importKerasSequentialModelAndWeights(
+			// new ClassPathResource(ECO_SAVIOR_MODEL_H5).getFile().getPath());
+
+			MultiLayerNetwork network = KerasModelImport.importKerasSequentialModelAndWeights(
+					"/Users/hbhavsar/git/salvation-iot-ws/src/main/resources/trained_models/eco-savior-model.json",
+					"/Users/hbhavsar/git/salvation-iot-ws/src/main/resources/trained_models/eco-savior-model.h5");
+			return network;
+		} catch (Exception e) {
+			log.error("Failed to load ML Model {}", e);
+			throw new RuntimeException("Failed to load Eco Savior model:" + e.getMessage());
+		}
 	}
 
 }
